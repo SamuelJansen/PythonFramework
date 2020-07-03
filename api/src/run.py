693a1ada8c154716d7a3e0f7aa6c00ae512d9924
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-07-03 03:04:08 -0300

    feat: globals package distribution

--- api/src/run.py
+++ api/src/run.py
@@ -1,30 +1,15 @@
+from globals import Globals
+globals = Globals(__file__,
+    debugStatus = True,
+    warningStatus = True,
+    errorStatus = True,
+    successStatus = True,
+    failureStatus = True,
+    settingStatus = True
+)
+
 def pythonFrameworkDefaultRun(commandList,globals,**kwargs):
     globals.debug(f'"pythonFrameworkDefaultRun()" method not implemented')
-
-try :
-    from globals import Globals
-    globals = Globals(
-        filePath = __file__,
-        debugStatus = True,
-        warningStatus = True,
-        errorStatus = True,
-        successStatus = True,
-        failureStatus = True,
-        settingStatus = True
-    )
-except Exception as exception :
-    print(exception)
-    from service.framework.globals import Globals
-    Globals = Globals.Globals
-    globals = Globals(
-        file = __file__,
-        debugStatus = True,
-        warningStatus = True,
-        errorStatus = True,
-        successStatus = True,
-        failureStatus = True,
-        settingStatus = True
-    )
 
 if __name__ == '__main__' :
     import PythonFramework
