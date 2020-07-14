87466267779e347fabe72a930a824f498ef103ab
Samuel Jansen <samuel.jansen@cwi.com.br>
2020-07-14 19:58:37 -0300

    wip: debugging deploye error

--- app.py
+++ app.py
@@ -1,27 +1,40 @@
-from globals import Globals
 
-print('[app] globals instanciating')
-globals = Globals(__file__,
-    successStatus = True,
-    settingStatus = True,
-    debugStatus = True,
-    warningStatus = True,
-    failureStatus = True,
-    errorStatus = True
-)
-print('[app] globals instanciated')
+try :
+    import PythonFrameworkFlask
+    print(f'[app] PythonFrameworkFlask imported')
 
-print(f'[app] PythonFrameworkFlask importing')
-import PythonFrameworkFlask
-print(f'[app] PythonFrameworkFlask imported')
+    print(f'[app] PythonFrameworkFlask.app refferenceing')
+    app = PythonFrameworkFlask.app
+    print(f'[app] PythonFrameworkFlask.app refferenced')
 
-print(f'[app] PythonFrameworkFlask.app refferenceing')
-app = PythonFrameworkFlask.app
-print(f'[app] PythonFrameworkFlask.app refferenced')
+    print(f'[app] PythonFrameworkFlask.api refferenceing')
+    api = PythonFrameworkFlask.api
+    print(f'[app] PythonFrameworkFlask.api refferenced')
+except :
+    from globals import Globals
 
-print(f'[app] PythonFrameworkFlask.api refferenceing')
-api = PythonFrameworkFlask.api
-print(f'[app] PythonFrameworkFlask.api refferenced')
+    print('[app] globals instanciating')
+    globals = Globals(__file__,
+        successStatus = True,
+        settingStatus = True,
+        debugStatus = True,
+        warningStatus = True,
+        failureStatus = True,
+        errorStatus = True
+    )
+    print('[app] globals instanciated')
+
+    print(f'[app] PythonFrameworkFlask importing')
+    import PythonFrameworkFlask
+    print(f'[app] PythonFrameworkFlask imported')
+
+    print(f'[app] PythonFrameworkFlask.app refferenceing')
+    app = PythonFrameworkFlask.app
+    print(f'[app] PythonFrameworkFlask.app refferenced')
+
+    print(f'[app] PythonFrameworkFlask.api refferenceing')
+    api = PythonFrameworkFlask.api
+    print(f'[app] PythonFrameworkFlask.api refferenced')
 
 if __name__ == '__main__' :
     print(f'[app] run()')
